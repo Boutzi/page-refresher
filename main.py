985fd4b70e77c74159a66cb4b7615d319379087a
Boutzi <joe@arkaans.com>
2024-12-19 15:54:20 +0100

    refacto

--- main.py
+++ main.py
@@ -21,6 +21,17 @@ def refreshPage(driver):
   driver.refresh()
   return driver.page_source
   
+prefs = {
+  # 2: Block all images (and video elements)
+  "profile.managed_default_content_settings.images": 2, 
+  # 2: Block notifications
+  "profile.default_content_setting_values.notifications": 2, 
+  # 2: Disable stylesheets
+  "profile.managed_default_content_settings.stylesheets": 2, 
+  # 2: Block JavaScript
+  "profile.managed_default_content_settings.javascript": 2  
+}
+
 def main():
   global stop
   options = Options()
@@ -32,9 +43,7 @@ def main():
   user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36"
   options.add_argument(f"user-agent={user_agent}")
   
-  # Disable images to speed up the process by uncommenting the following lines
-  # prefs = {"profile.managed_default_content_settings.images": 2}
-  # options.add_experimental_option("prefs", prefs)
+  options.add_experimental_option("prefs", prefs)
 
   caps = DesiredCapabilities().CHROME
   caps["pageLoadStrategy"] = "eager"
@@ -45,14 +54,19 @@ def main():
   listener = keyboard.Listener(on_press=on_press)
   listener.start()
 
-  print(f'{"\033[31m"}Press "Esc" to stop the script\n{"\033[0m"}')
+  print(f'{"\033[36m"}Press "Esc" to stop the script\n{"\033[0m"}')
 
   try:
     while not stop:
       start_time = time.time()
       refreshPage(driver)
       end_time = time.time()
-      print(f"Page refreshed in {end_time - start_time:.2f} seconds")
+      if end_time - start_time > 2:
+        print(f"{"\033[31m"}Page refreshed in {end_time - start_time:.2f} seconds{"\033[0m"}")
+      elif end_time - start_time > 1:
+        print(f"{"\033[33m"}Page refreshed in {end_time - start_time:.2f} seconds{"\033[0m"}")
+      else:
+        print(f"{"\033[32m"}Page refreshed in {end_time - start_time:.2f} seconds{"\033[0m"}")
   finally:
     listener.stop()
     driver.quit()
